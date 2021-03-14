25a887c111ba85aa35ea45b8dbafb3dff15f8d7f
Maruf Abdullaev <m.abdullaev4188@gmail.com>
2021-03-14 11:54:29 -0400

    Add project files.

---
+++ AlifLesson16HW13.03/Models/Manipul.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlifLesson16HW13._03.Models
+{
+    class Manipul
+    {
+        public int[] reverse(int[] arr) { return arr.Reverse().ToArray(); }
+
+        public string[] countByAcending(string[] arr) { return arr.OrderBy(t => t.Length).ToArray(); }
+
+        public int[] sumAndCount(int[] arr) 
+        {
+            int sum = arr.Where(t => t < 0).Sum();
+            int count = arr.Where(t => t > 0).Count();
+            int[] array = { count, sum };
+            return array;
+        }
+
+        public int[] uniqueNum(int[] arr)
+        {
+            return (arr.GroupBy(i => i).Where(i => i.Count() == 1).Select(i => i.Key)).ToArray();
+        }        
+    }
+}
---
+++ AlifLesson16HW13.03/Program.cs
@@ -0,0 +1,39 @@
+﻿using AlifLesson16HW13._03.Models;
+using System;
+
+namespace AlifLesson16HW13._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, 4, 5, 6 };
+            Manipul manipulation = new Manipul();
+            int[] numArray = manipulation.reverse(num);
+            foreach (var item in numArray) Console.WriteLine(item);
+
+            Console.WriteLine("---------------");
+
+            string[] fruits = { "Apple", "Grape", "Rasberry", "Blackberry" };
+            string[] fruitsArray = manipulation.countByAcending(fruits);
+            foreach (var item in fruitsArray) Console.WriteLine(item);
+
+            Console.WriteLine("---------------");
+
+            int[] num2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15 };            
+            foreach (var item in manipulation.sumAndCount(num2))
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("---------------");
+
+            int[] num3 = { 1, 1, 1, 1, 2, 1, 1 };
+            int[] num3Array = manipulation.uniqueNum(num3);
+            foreach (var item in num3Array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
